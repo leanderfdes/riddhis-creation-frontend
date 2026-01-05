4e6551995949bbb00c47107cfe9b04b405df819b
Leander Fernandes <leanderfdes22@gmail.com>
2026-01-05 23:35:49 +0530

    changed useCdn

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -4,5 +4,5 @@ export const sanity = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: "production",
   apiVersion: "2024-01-01",
-  useCdn: true
+  useCdn: false
 });
